aca54e6f83707eb1eefcc4941596d251fa2735ef
Kwiatek90 <mateusz.kwiat9@gmail.com>
2023-10-26 09:16:33 +0200

    socket

---
+++ Python/SocketAndJson/Echo_client_and_server/server.py
@@ -0,0 +1,21 @@
+# echo server
+
+import socket
+
+HOST = "127.0.0.1" # Standard loopback interface addres (localhost)
+PORT = 65432 # Port to listen on  (non-privileged ports are > 1023)
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT)) #towrzy serwer
+    s.listen() # nasluchuje czy ktos sie laczy
+    conn, adr = s.accept() #pobiera adrress i conn z polaczenia
+    with conn:
+        print(f"Connected: {adr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
+            
+        

5c7f5100caab6378da9b9658969a48a99211d1be
Kwiatek90 <mateusz.kwiat9@gmail.com>
2023-12-29 09:12:20 +0100

    multi

--- Python/Wielowatkowosc_v2/multiprocessing_ex1.py
+++ Python/Wielowatkowosc_v2/multiprocessing_ex1.py
@@ -1,22 +1,33 @@
 import time 
-import multiprocessing
+import concurrent.futures
 
 start = time.perf_counter()
 
-def do_smth():
-    print(f"Sleeping 1 second(s) ...")
-    time.sleep(1)
-    print("Done sleeping")
+def do_smth(seconds):
+    print(f"Sleeping {seconds} second(s) ...")
+    time.sleep(seconds)
+    return "Done sleeping"
+
+if __name__ == '__main__':  
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        secs = [5,4,3,2,1]
+        results = executor.map(do_smth, secs)
+        
+        for result in results:
+            print(result)
+            
+    finish = time.perf_counter()
+
+    print(f"Finished in {round(finish-start,2)} second(s)") 
   
-p1 = multiprocessing.Process(target=do_smth)
-p2 = multiprocessing.Process(target=do_smth)
-
-p1.start()
-p2.start()
-
-p1.join()
-p2.join()
-    
-finish = time.perf_counter()
-
-print(f"Finished in {round(finish-start,2)} second(s)")
+#processes = [] 
+  
+#if __name__ == '__main__':  
+#    for _ in range(10):
+#        p = multiprocessing.Process(target=do_smth, args=[1.5])
+#        p.start()
+#        processes.append(p)
+#        
+#    for process in processes:
+#         process.join()   
+#        
